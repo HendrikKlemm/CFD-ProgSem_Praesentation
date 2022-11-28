2826ed3cffc909d1dbd5c06b07af6162874b58c2
Hendrik Klemm <hendrik.klemm@online.de>
2022-11-28 18:51:53 +0100

    Update x_axis_inverter.py

--- Hendrik/invert_x_axis/x_axis_inverter.py
+++ Hendrik/invert_x_axis/x_axis_inverter.py
@@ -1,15 +1,42 @@
 import json
 import pandas as pd
 
-with open("filenames_for_inversion.json", "r") as f:
-    file_list = json.load(f)
-    file_list = file_list["files"]
+# INPUTS
+json_file_name = "filenames_for_inversion.json" # contains .csv files
 
-for file in file_list:
-    df_in = pd.read_csv(file)
-    print(df_in.head())
+#FUNCTIONS
+def get_file_list(json_file_name):
+    """returns list of files specified in the json file
+
+    Arguments:
+    json_file_name: str, file name of json file
     
-    df_out = df_in.reindex(index=df_in.index[::-1])
-    print(df_out.head())
+    Returns: 
+    file_list: list of strings, csv file names
+    """
+    with open(json_file_name, "r") as f:
+        file_list = json.load(f)
+        file_list = file_list["files"]
+    return file_list
+
+def invert_rows(file_list):
+    """loops over csv files in file_list and inverts rows
+
+    Arguments:
+    file_list: list of strings, csv file names
     
-    df_out.to_csv('out_'+file, sep=',', index=0, index_label=0)
+    Returns:
+    
+    """
+    for file in file_list:
+        df_in = pd.read_csv(file)
+        print(df_in.head())
+        
+        df_out = df_in.reindex(index=df_in.index[::-1])
+        print(df_out.head())
+        
+        df_out.to_csv('out_'+file, sep=',', index=0, index_label=0)
+
+if __name__ == '__main__':
+    file_list = get_file_list(json_file_name)
+    invert_rows(file_list)
